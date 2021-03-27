6e85e0fa0768a678a728605325b17122c2aad652
alexa.kearns <alexa.kearns@potatolondon.com>
2021-03-27 20:41:11 +0000

    Add Nasa API fetch and display some results

--- src/components/widgets/One.vue
+++ src/components/widgets/One.vue
@@ -1,3 +1,44 @@
 <template>
-  <div class="widget-one"></div>
+  <div class="widget-one">
+    <h2 class="widget-one__header">NASA</h2>
+    <div v-bind="info" >
+      <img v-bind:src="info.url" class="widget-one__image">
+    </div>
+    <div v-bind="info">{{info.title}}</div>
+    <div v-bind="info">{{info.date}}</div>
+
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'WidgetOne',
+  data () {
+    return {
+      info: {}
+    }
+  },
+  mounted: function() {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=`)
+      .then(response => response.json())
+      .then(data => this.info = data)
+      .catch(error => console.log(error))
+  },
+}
+</script>
+
+<style type="text/css">
+  .widget-one {
+    border: solid 1px black;
+
+	}
+	
+	.widget-one__header {
+		text-align: center;
+	}
+
+  .widget-one__image {
+    max-width: 400px;
+  }
+</style>
+
