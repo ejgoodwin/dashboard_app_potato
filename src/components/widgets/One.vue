eb4faef09c7f14f1c892d9274a168c373b9691fc
alexa.kearns <alexa.kearns@potatolondon.com>
2021-04-13 19:01:34 +0100

    Add date/title styling and include info text

--- src/components/widgets/One.vue
+++ src/components/widgets/One.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="widget-one">
     <h2 class="widget-one__header">NASA</h2>
+    <div v-bind="info" class="widget-one__title">{{info.title}}</div>
     <div v-bind="info" >
       <img v-bind:src="info.url" class="widget-one__image">
+      <p v-bind="info">{{info.explanation}}</p>
     </div>
-    <div v-bind="info">{{info.title}}</div>
-    <div v-bind="info">{{info.date}}</div>
-
+    <div v-bind="info" class="widget-one__date">{{info.date}}</div>
   </div>
 </template>
 
 <script>
+
+console.log(import.meta.env)
 export default {
   name: 'WidgetOne',
   data () {
@@ -19,7 +21,7 @@ export default {
     }
   },
   mounted: function() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_NASA_API_KEY}`)
       .then(response => response.json())
       .then(data => this.info = data)
       .catch(error => console.log(error))
@@ -30,15 +32,23 @@ export default {
 <style type="text/css">
   .widget-one {
     border: solid 1px black;
-
 	}
 	
 	.widget-one__header {
 		text-align: center;
 	}
 
+  .widget-one__title {
+    text-align: center;
+  }
+
   .widget-one__image {
     max-width: 400px;
+  }
+
+  .widget-one__date {
+    text-align: right;
+    font-style: italic;
   }
 </style>
 
