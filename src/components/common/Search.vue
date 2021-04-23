ab97d31104cdf2f975736e527661e15dfea08d22
Purvi Trivedi <purvi.trivedi@potatlondon.com>
2021-04-23 16:09:28 +0100

    Early work

---
+++ src/components/common/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="search-form"
+  @submit.prevent="search"
+  >
+    <input
+      class="search-form__input"
+      type="text"
+      :placeholder="placeholder"
+      :value="searchText"
+      @input="(event) => $emit('update:searchText', event.target.value)"
+    />
+    <button type="submit" class="search-form__input-btn">
+      <i class="fas fa-search"></i>
+    </button>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: "Search",
+  props: {
+    'placeholder': String,
+    'searchText':String,
+    'search': Function,
+  }
+};
+</script>
--- src/components/widgets/Two.vue
+++ src/components/widgets/Two.vue
@@ -1,3 +1,49 @@
 <template>
-	<div></div>
+	<div class="maps">
+        <Search
+        placeholder="Search for a city"
+        v-model:searchText="searchText"
+        :search="searchLocation"
+        />
+        <div id="map">
+          <!-- <mapbox
+          access-token="pk.eyJ1IjoicHVydml0cml2ZWRpIiwiYSI6ImNrYTU1Z3RnYzEwcHgza3AwYTU3c2h2ZDMifQ.3ZFchMC0MNZwHbY2Dz2MMA"
+          :map-options="{
+            style: 'mapbox://styles/mapbox/light-v9',
+            center: [-96, 37.8],
+            zoom: 3,
+            }"
+    /> -->
+        </div>
+    </div>
 </template>
+
+<script>
+import Search from '../common/Search.vue';
+
+export default {
+    name: 'Two',
+    data: () => ({
+        searchText: '',
+    }),
+    components: {
+        Search
+    },
+    methods: {
+        searchLocation() {
+            console.log(this.searchText)
+        },
+    },
+}
+</script>
+
+<style>
+.maps {
+    border: 1px solid blue;
+}
+
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
