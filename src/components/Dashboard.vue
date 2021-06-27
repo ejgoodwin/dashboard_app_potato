845985bc0b633ffef8f42f9c403c41649b52998c
ejgoodwin <ericagoodwin90@gmail.com>
2021-06-27 13:45:03 +0100

    added svg bar chart

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="dashboard">
-  	<one class="card"></one>
-  	<two class="card"></two>
-  	<three class="card"></three>
-  </div>
+	  <div class="dashboard">
+	  	<one class="card"></one>
+	  	<two class="card"></two>
+	  	<three class="card"></three>
+	  	<four class="card"></four>
+	  </div>
 </template>
 
 <script>
 import One from './widgets/One.vue';
 import Two from './widgets/Two.vue';
 import Three from './widgets/Three.vue';
+import Four from './widgets/Four.vue';
 
 export default {
   name: 'Dashboard',
   components: {
   	'one': One,
   	'two': Two,
-  	'three': Three
+  	'three': Three,
+  	'four': Four
   }
 }
 </script>
@@ -25,7 +28,6 @@ export default {
 <style type="text/css">
 	.dashboard {
 		display: grid;
-		grid-template-rows: 1fr 1fr;
 		grid-template-columns: 1fr 1fr;
 		grid-gap: 16px;
 		padding: 16px;
